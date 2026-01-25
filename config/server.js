376851b4f00a8db39572fdd1e51560d1fc7b66cf
Entush <88847021+Entush@users.noreply.github.com>
2026-01-25 23:06:38 +0700

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,10 +1,4 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-
-  admin: {
-    auth: {
-      secret: env('STRAPI_ADMIN_JWT_SECRET'),
-    },
-  },
 });
